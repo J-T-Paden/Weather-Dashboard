e5beb452f1540fb9ee2bfae8004b8a896bde355e
Jared P <jared.t.paden@gmail.com>
2023-09-30 17:23:55 -0600

    Updated HTML, CSS, added JS, DayJS and WeatherAPI

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,13 @@
-console.log("JavaScript is working!")
+var dateEl = document.getElementById("cityDateEl")
+var searchButton = document.getElementById("searchButton")
+var requestUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={8e70090a850bcd7ae5f862ff86cd1fcf}"
+console.log(dateEl)
+dateEl.innerHTML = "City and " + dayjs().format('MMM, DD, YYYY')
+
+searchButton.addEventListener("click", search)
+function search() {
+
+fetch(requestUrl)
+    .then(function (response) {
+    return response.json();
+    })}
